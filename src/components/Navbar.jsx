1e3df3ffdeaad1970a4168285845dd535972f496
Arina Siedina <orynasiedina@gmail.com>
2023-09-27 15:35:31 +0200

    Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, { useState} from 'react'
+import {Link} from "react-router-dom";
+import {styles} from '../styles'
+import {navLinks} from "../constants";
+import {logo, menu, close} from "../assets";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    const [active, setActive] = useState('');
+    const [toggle, setToggle] = useState(false);
+
+    const navBarMenu = (position) => {
+        return (
+            <ul className=
+                    {position === 'main'
+                        ? 'list-none hidden sm:flex flex-row gap-10'
+                        : 'list-none flex justify-end items-start flex-col gap-4'
+                    }>
+                {navLinks.map((link) => (
+                    <li
+                        key={link.id}
+                        className={`${
+                            active === link.title
+                                ? "text-white"
+                                : "text-secondary"
+                        } 
+                        ${position === 'main'
+                            ? 'hover:text-white text-[18px] font-medium cursor-pointer'
+                            : 'font-poppins font-medium cursor-pointer text-[16px]'
+                        }`}
+
+                        onClick={() => {
+                            if (position === 'mobile') {
+                                setToggle(!toggle);
+                            }
+                            setActive(link.title)
+                        }}
+                    >
+                        <a href={`#${link.id}`}>{link.title}</a>
+                    </li>
+                ))}
+            </ul>
+        )
+    }
+
+    return (
+        <nav className={`${styles.paddingX} w-full items-center py-5 top-0 z-20 bg-primary`}>
+            <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+                <Link
+                    to="/"
+                    className="flex items-center gap-2"
+                    onClick={() => {
+                        setActive("");
+                        window.scrollTo(0, 0);
+                    }}>
+                    <img src={logo} alt='logo' className="w-12 h-12 object-contain"/>
+                    <p className="text-cyan-300 text-[18px] font-bold cursor-pointer flex">Arina &nbsp;<span
+                        className="sm:block hidden">| Junior Developer</span></p>
+                </Link>
+                {navBarMenu("main")}
+                <div className="sm:hidden flex flex-1 justify-end items-center">
+                    <img src={toggle ? close : menu} alt="menu"
+                         className="w=[28px] h-[28px] object-contain cursor-pointer"
+                         onClick={() => setToggle(!toggle)}/>
+                    <div
+                        className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+                        {navBarMenu("mobile")}
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
---
+++ src/constants/index.js
@@ -0,0 +1,232 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  gf, ph, DTS, penta,
+} from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "React Native Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "Content Creator",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Full Stack Developer BootCamp",
+      company_name: "Green Fox Academy",
+      icon: gf,
+      iconBg: "white",
+      date: "March 2023 - September 2023",
+      points: [
+        "Developing and maintaining web applications using different technologies.",
+        "Collaborating with mentors and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "Front Office Manager",
+      company_name: "Pinelli Hotels",
+      icon: ph,
+      iconBg: "white",
+      date: " 2022 - 2023",
+      points: [
+        "Supervise front office staff, handle training, scheduling, and performance evaluation.",
+        "Address guest issues, maintain up-to-date room status, and ensure operational smoothness.",
+        "Optimize revenue and occupancy through rate monitoring, credit reports, and policy enforcement.",
+      ],
+    },
+    {
+      title: "Operations Manager",
+      company_name: "DownTown Suites Prague",
+      icon: DTS,
+      iconBg: "#E95901",
+      date: "2020 - 2022",
+      points: [
+        "Supervise all departments, support Heads of Departments for effective operation.",
+        "Enforce SOPs, conduct inspections for quality and service standards.",
+        "Control operational costs, budgeting, address customer feedback, and manage service recovery promptly.",
+      ],
+    },
+    {
+      title:"Guest Services Supervisor",
+      company_name: "Penta Hotels",
+      icon: penta,
+      iconBg: "#1F1919",
+      date: "2018 - 2020",
+      points: [
+        "Supervising and leading a team of Guest Service Agents, ensuring the delivery of exceptional customer service.",
+        "Monitoring and maintaining front desk operations, including check-ins, check-outs, and reservations.",
+        "Resolving guest issues, providing guidance to staff, and assisting in maintaining a high standard of service quality.",
+      ],
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Car Rent",
+      description:
+        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Job IT",
+      description:
+        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Trip Guide",
+      description:
+        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, projects };
